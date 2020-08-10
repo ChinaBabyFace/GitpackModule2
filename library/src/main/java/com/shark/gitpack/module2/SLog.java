60f60a8ede44a6eaec5d20d1ef0f4b1c0d939b2e
renyuxiang <inankai@sina.com>
2020-08-10 12:02:57 +0800

    新增一个方法

--- library/src/main/java/com/shark/gitpack/module2/SLog.java
+++ library/src/main/java/com/shark/gitpack/module2/SLog.java
@@ -6,4 +6,8 @@ public class SLog {
     public static void e(Object target, String message) {
         Log.e(target.getClass().getSimpleName(), message);
     }
+
+    public static void d(Object target, String message) {
+        Log.d(target.getClass().getSimpleName(), message);
+    }
 }
